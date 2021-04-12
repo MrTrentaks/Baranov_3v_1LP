667a29b898525e9fef3fb5165cae1e6e7ab9b2c5
Baranov <vspc_vs@VSPC34.RU>
2021-04-12 12:35:33 +0400

    11

---
+++ Баранов/Баранов.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+
+long int gcd(const long int x, const long int y)
+{
+    return (y == 0 ? x : gcd(y, x % y));
+}
+
+int main()
+{
+    int x, y;
+
+    cout << "Input 2 numbers: ";
+    cin >> x >> y;
+    cout << "gcd = " << gcd(x, y) << endl;
+    _getch();
+    return 0;
+
+}
